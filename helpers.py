b706848122a71f09c93a140aa9311fe283c0136c
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-05-08 21:46:46 -0700

    Start drawing board and tile class

--- helpers.py
+++ helpers.py
@@ -1,2 +1,10 @@
+class tile:
+    def __init__(self, x=0, y=0, d=0, value=0):
+        self.x = x
+        self.y = y
+        self.d = d
+        self.value = value
+
+
 def initial_state():
     return [[0 for _ in range(9)] for _ in range(9)]
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import helpers as h
 
 pg.init()
 clock = pg.time.Clock()
-width, height = 600, 700
+width, height = 800, 800
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -22,10 +22,22 @@ moveFont.bold = True
 board = h.initial_state()
 
 
+def draw_board():
+    dim = width - 100
+    tile_w = dim/9
+    tile_h = dim/9
+
+    board_back = pg.Rect(0, 0, dim, dim)
+    board_back.center = (width // 2, height // 2)
+    pg.draw.rect(screen, white, board_back, border_radius=5)
+
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
+
+    draw_board()
 
     clock.tick(60)
     pg.display.flip()
